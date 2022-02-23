85707371197cb0d089571df29b31da1d45c8f330
FlareStormGaming <70780971+FlareStormGaming@users.noreply.github.com>
2022-02-22 21:47:14 -0800

    New Sources! A ton of them.

---
+++ build_data.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+
+def rem_banned(words):
+    out = []
+    for word in words:
+        stripped_word = word
+        for banned in ['\n', ',', '.', '!', '?', '"', '”', '“', '-', '—', ']', '[', ':', ';']:
+            stripped_word = stripped_word.replace(banned, '')
+        out.append(stripped_word.lower())
+    return out
+
+
+files = os.listdir("./sources/")
+
+data = []
+for i in files:
+    with open(f"sources/{i}", 'r') as f:
+        data.extend(rem_banned(f.read().split()))
+
+keys = list(set(data))
+
+datadict = dict.fromkeys(keys)
+
+for i in keys:
+    datadict[i] = {}
+
+for ind, word in enumerate(data):
+    word_data = datadict[word]
+    try:
+        word_data[data[ind + 1]] += 1
+    except KeyError:
+        word_data[data[ind + 1]] = 1
+    except IndexError:
+        pass
+
+print(datadict["glory"])
